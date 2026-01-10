58fd1cde9658f65748343aeb6ed79c61e687f70d
Lastram636 <piokamin@gmail.com>
2026-01-10 20:19:11 +0100

    bad test

--- py-upper/test_uppercase.py
+++ py-upper/test_uppercase.py
@@ -2,7 +2,7 @@ import pytest
 from uppercase import to_upper
 
 def test_to_uppercase():
-    assert to_upper("hello") == "HELLO"
+    assert to_upper("hello") == "HELLOa"
     assert to_upper("Python") == "PYTHON"
     assert to_upper("123abc") == "123ABC"
     assert to_upper("") == ""  # Test pustego napisu
